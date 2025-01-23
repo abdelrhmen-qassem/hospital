7bb40f1fc3324661a73863877a911635c7ed5cbc
abdelrhmen-qassem <bq868704@gmail.com>
2025-01-23 11:45:10 +0200

    Your commit message

--- resources/views/dashboard/user/auth/signin.blade.php
+++ resources/views/dashboard/user/auth/signin.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.master2')
+@extends('dashboard.layouts.master2')
 @section('css')
 <!-- Sidemenu-respoansive-tabs css -->
-<link href="{{URL::asset('assets/plugins/sidemenu-responsive-tabs/css/sidemenu-responsive-tabs.css')}}" rel="stylesheet">
+<link href="{{URL::asset('Dashboard/plugins/sidemenu-responsive-tabs/css/sidemenu-responsive-tabs.css')}}" rel="stylesheet">
 @endsection
 @section('content')
 		<div class="container-fluid">
@@ -10,7 +10,7 @@
 				<div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
 					<div class="row wd-100p mx-auto text-center">
 						<div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
-							<img src="{{URL::asset('assets/img/media/login.png')}}" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
+							<img src="{{URL::asset('Dashboard/img/media/login.png')}}" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
 						</div>
 					</div>
 				</div>
@@ -22,17 +22,17 @@
 							<div class="row">
 								<div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
 									<div class="card-sigin">
-										<div class="mb-5 d-flex"> <a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="sign-favicon ht-40" alt="logo"></a><h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Va<span>le</span>x</h1></div>
+										<div class="mb-5 d-flex"> <a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('dashboard/img/brand/favicon.png')}}" class="sign-favicon ht-40" alt="logo"></a><h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Va<span>le</span>x</h1></div>
 										<div class="card-sigin">
 											<div class="main-signup-header">
 												<h2>Welcome back!</h2>
 												<h5 class="font-weight-semibold mb-4">دخول المستخدم</h5>
                                                 <form method="POST" action="{{ route('login.user') }}">
 													<div class="form-group">
-														<label>Email</label> <input class="form-control" placeholder="Enter your email" type="text">
+														<label>Email</label> <input class="form-control" name="email" placeholder="Enter your email" type="email" :value="old('email')" required autofocus />
 													</div>
 													<div class="form-group">
-														<label>Password</label> <input class="form-control" placeholder="Enter your password" type="password">
+														<label>Password</label> <input class="form-control" placeholder="Enter your password"  type="password"  name="password" required autocomplete="current-password">
 													</div><button class="btn btn-main-primary btn-block">Sign In</button>
 													<div class="row row-xs">
 														<div class="col-sm-6">
